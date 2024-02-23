06d8669de9b3fb649bf5cbb101ab2f861e9cf3dc
Kerr Hamilton <kerr9928@gmail.com>
2024-02-23 12:58:29 +0000

    Add Javascript functions to start quiz and rules? buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-console.log("connected");
+// code for turning Start quiz? button yellow or grey
+function turnYellow() {
+    let begin = document.getElementById("start");
+    begin.style.backgroundColor = "yellow";
+}
+
+function turnGrey() {
+    let begin = document.getElementById("start");
+    begin.style.backgroundColor = "rgb(130, 139, 157)"
+}
+
+// code for turning rules? button yellow or grey 
+
+function turnYellow1() {
+    let begin = document.getElementById("rules");
+    begin.style.backgroundColor = "yellow";
+}
+
+function turnGrey1() {
+    let begin = document.getElementById("rules");
+    begin.style.backgroundColor = "rgb(130, 139, 157)"
+}
+
