8d0d548416e5092cfdfc30986b18af3c6f1c13d9
Kerr Hamilton <kerr9928@gmail.com>
2024-02-28 22:16:35 +0000

    create page for quiz layout and add css styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,39 @@ function displayOn() {
 // code for start button to display questions
 // simple step code
 
+const question = document.getElementById("quizQuestions")
+const choices = Array.from(document.getElementsByClassName("quizChoices"));
+
+let currentquestion = {};
+let acceptingAnswers = true;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question: 'When was old Trafford built?',
+        choice1: '1910',
+        choice2: '1950',
+        choice3: '2010',
+        choice4: '1990',
+        answer: 1,
+    },
+
+    {
+        question: 'Who is Manchester Uniteds all-time top scorer?',
+        choice1: 'Cristiano Ronaldo',
+        choice2: 'Ryan Giggs',
+        choice3: 'Wayne Rooney',
+        choice4: 'Dennis Law',
+        answer: 3,
+    },
+
+    {
+        question: 'What Family currently own the biggest share of Manchester United?',
+        choice1: 'The Glazers',
+        choice2: 'The busbys',
+        choice3: 'The Nevilles',
+        choice4: 'The Ferdinands',
+        answer: 1,
+    },
+]
