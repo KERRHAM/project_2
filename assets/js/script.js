d81379b23146e2b66d66f44e1cb0e8cffaadd2ba
Kerr Hamilton <kerr9928@gmail.com>
2024-03-02 22:56:21 +0000

    add 15 questions to questions variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,51 +65,181 @@ var questions = [
     
     { Question: "What Year was old trafford built?",
       Answers: {
-        A:"A:1990", 
-        B:"B:1910", 
-        C:"C:2005", 
-        D:"D:1905"
+        A:"A: 1990", 
+        B:"B: 1910", 
+        C:"C: 2005", 
+        D:"D: 1905"
       },
         correctAnswer: "B"
     },
 
     { Question: "Alex Ferguson famously kicked a boot at which player, injuring his eye?",
       Answers: {
-        A:"Ryan Giggs", 
-        B:"David Beckham", 
-        C:"Eric Cantona", 
-        D:"Wayne Rooney"
+        A:"A: Ryan Giggs", 
+        B:"B: David Beckham", 
+        C:"C: Eric Cantona", 
+        D:"D: Wayne Rooney"
       },
         correctAnswer: "B"
     },
 
     { Question: "Who scored the winning goal for Manchester United in their 2-1 Champions League final victory in 1999?",
       Answers:{
-        A:"Gary Nevile", 
-        B:"Ryan Giggs", 
-        C:"Eric Cantona", 
-        D:"Ole Gunnar Solskjaer"},
+        A:"A: Gary Nevile", 
+        B:"B: Ryan Giggs", 
+        C:"C: Eric Cantona", 
+        D:"D: Ole Gunnar Solskjaer"},
         correctAnswer: "D"
     },
-    
-    
 
+    { Question: "Which player is affectionately known as 'The King' to Manchester United fans?",
+      Answers: {
+        A:"A: Eric Cantona ", 
+        B:"B: Watne Rooney ", 
+        C:"C: Cristiano Ronaldo", 
+        D:"D: Alex Ferguson"
+      },
+        correctAnswer: "A"
+    },
+
+    { Question: "Manchester United broke their record transfer fee in 2016 when they paid £89.5 million for which player?",
+    Answers: {
+      A:"A: Paul Pogba", 
+      B:"B: Alexis Sanchez", 
+      C:"C: Memphis Depay", 
+      D:"D: Henrikh Mkhitaryan"
+    },
+      correctAnswer: "A"
+    },
+
+    { Question: "As of 2020, Manchester United hold the record for most league titles in England. How many times have they been champions?",
+    Answers: {
+      A:"A: 20", 
+      B:"B: 15", 
+      C:"C: 22", 
+      D:"D: 10"
+    },
+      correctAnswer: "A"
+  },
+
+    { Question: "Which player holds the record for most Man Utd appearances?",
+    Answers: {
+      A:"A: Roy Keane", 
+      B:"B: Ryan Giggs", 
+      C:"C: Bobby Charlton", 
+      D:"D: Wayne Rooney"
+    },
+      correctAnswer: "B"
+  },
+
+  { Question: "Which player holds the record for most Man Utd goals?",
+    Answers:{
+      A:"A: Cristiano Ronaldo", 
+      B:"B: Wayne Rooney", 
+      C:"C: Eric Cantona", 
+      D:"D: Ryan Giggs"},
+      correctAnswer: "B"
+  },
+
+  { Question: "The highest transfer fee ever received by Man Utd was £80 million in 2009 - for which player?",
+    Answers: {
+      A:"A: Cristiano Ronaldo", 
+      B:"B: Carlos Teves", 
+      C:"C: David Beckham", 
+      D:"D: Romelu Lukaku "
+    },
+      correctAnswer: "A"
+  },
+
+  { Question: "Who were Manchester United's opponents in the first game at Old Trafford?",
+  Answers: {
+    A:"A: Everton", 
+    B:"B: Middlesborough", 
+    C:"C: Liverpool", 
+    D:"D: Man City"
+  },
+    correctAnswer: "C"
+  },
+
+  { Question: "What is the capacity of Old Trafford (as of 2020)?",
+  Answers: {
+    A:"A: 70,000", 
+    B:"B: 74,000 ", 
+    C:"C: 68,000", 
+    D:"D: 65,000"
+  },
+    correctAnswer: "B"
+  },
+
+  { Question: "What is the nickname of Old Trafford?",
+  Answers: {
+    A:"A: The Theatre of Dreams", 
+    B:"B: The Theatre of Manchester", 
+    C:"C: The Theatre of success", 
+    D:"D: The home of Dreams"
+  },
+    correctAnswer: "A"
+  },
+
+  { Question: "Old Trafford is situated on what road?",
+  Answers: {
+    A:"A: Sir Matt Busby Way.", 
+    B:"B: Sir Ferguson Way", 
+    C:"C: Rooney road", 
+    D:"D: Giggs Street"
+  },
+    correctAnswer: "A"
+  },
+
+  { Question: "Manchester United were the first British team to win the European Cup (now Champions League).",
+  Answers: {
+    A:"A: True?", 
+    B:"B: False?", 
+    C:" ", 
+    D:""
+  },
+    correctAnswer: "B"
+  },
+
+  { Question: "The West Stand of Old Trafford is more famously known as what?",
+  Answers: {
+    A:"A: The Stretford End", 
+    B:"B: The Busby End", 
+    C:"C: Charlton stand", 
+    D:"D: Sir Alex ferguson end"
+  },
+    correctAnswer: "A"
+  },
+
+
+  
+     
 ]
 
-function displayQuestion(questions) {
-    
-    document.getElementById("quizQuestions").innerHTML = questions[0].Question;
-    document.getElementById("quizAnswer1").innerHTML = questions[0].Answers.A
-    document.getElementById("quizAnswer2").innerHTML = questions[0].Answers.B
-    document.getElementById("quizAnswer3").innerHTML = questions[0].Answers.C
-    document.getElementById("quizAnswer4").innerHTML = questions[0].Answers.D
-}
-        
-displayQuestion(questions)
 
-function nextQuestion(questions) {
-    
+var currentQuestionIndex = 0;
+
+function displayQuestion(index) {
+    var question = questions[index];
+    document.getElementById("quizQuestions").innerHTML = question.Question;
+    document.getElementById("quizAnswer1").innerHTML = question.Answers.A;
+    document.getElementById("quizAnswer2").innerHTML = question.Answers.B;
+    document.getElementById("quizAnswer3").innerHTML = question.Answers.C;
+    document.getElementById("quizAnswer4").innerHTML = question.Answers.D;
 }
+
+function nextQuestion() {
+    
+    currentQuestionIndex++;
+    
+     if (currentQuestionIndex <= questions.length) {
+        displayQuestion(currentQuestionIndex);
+        } else {
+
+         console.log("end of Quiz")
+       }
+
+    }
 
 
 // results.html code
